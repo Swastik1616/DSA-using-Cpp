6eab16fdb9df04b8b61f098d9d071cd6ba416c97
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-24 12:56:07 +0530

    Fib_DP
    24 July 2025

--- Programs/basics/Fib2.cpp
+++ Programs/basics/Fib2.cpp
@@ -1,28 +1,37 @@
-// // Implementation of nth Fib using Recursion + Avoiding re-computations i.e. Dynamic Programming
+// Implementation of nth Fib using Recursion + Avoiding re-computations i.e. Dynamic Programming
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// int Fib (int n){
-//     if(n<=1){
-//         a[n]=1;
-//         return 1;
-//     }
-//     else
-//         return (Fib(n-1)+Fib(n-2));
-// }
-// int n;
-// int a[n];
-// int main(){
-//     cin >> n;
-//     for(int i = 0; i <= n; i++){
-//         a[i] = -1;
-//        // cout << a[i] << endl;
-//     }
-//     for(int i = 0; i <= n; i++){
-//         a[i] = Fib(i);
-//        // cout << a[i] << endl;
-//     }
-
-//     cout << "The value of " << n <<"th Fibonacci number is: " << a[n];
-// }
+int Fib (int n,int *p){
+    if(n<=1){
+        p[n]=1;
+        return 1;
+    }
+    else{
+        if(p[n-1]==-1){
+            p[n-1]=Fib(n-1,p);
+        }
+        if(p[n-2]==-1){
+            p[n-2]=Fib(n-2,p);
+        }
+        p[n]=p[n-1] + p[n-2];
+        return p[n];
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    int *p = a;
+    for(int i = 0; i <= n; i++){
+        a[i] = -1;
+       // cout << a[i] << endl;
+    }
+    Fib(n,a);
+    cout << "The Fibonacci series upto Fib[" << n <<"] is: " << endl;
+    for(int i = 0; i <= n; i++){
+       cout << a[i] << " ";
+    }
+    cout << endl << "And the value of " << n <<"th Fibonacci number is: " << a[n];
+}
