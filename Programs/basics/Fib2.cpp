d7499c40dfd6d00d817dba923e1f49eecb1941d0
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-24 13:01:30 +0530

    Update Fib2.cpp

--- Programs/basics/Fib2.cpp
+++ Programs/basics/Fib2.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 int Fib (int n,int *p){
-    if(n<=1){
+    if(n<=1){   // if n=0 or 1 base condition
         p[n]=1;
         return 1;
     }
-    else{
-        if(p[n-1]==-1){
-            p[n-1]=Fib(n-1,p);
+    // if n>1
+    else{ 
+        if(p[n-1]==-1){ //if this is true it means fib of this call has not come yet
+            p[n-1]=Fib(n-1,p); // so we call it now
         }
-        if(p[n-2]==-1){
-            p[n-2]=Fib(n-2,p);
+        if(p[n-2]==-1){ //if this is true it means fib of this call has not come yet
+            p[n-2]=Fib(n-2,p); // so we call it now
         }
-        p[n]=p[n-1] + p[n-2];
+        p[n]=p[n-1] + p[n-2]; 
         return p[n];
     }
 }
@@ -23,12 +24,15 @@ int main(){
     int n;
     cin >> n;
     int a[n];
-    int *p = a;
+    int *p = a; // for call by reference
+    //initialising the array
     for(int i = 0; i <= n; i++){
         a[i] = -1;
        // cout << a[i] << endl;
     }
     Fib(n,a);
+
+    //printing
     cout << "The Fibonacci series upto Fib[" << n <<"] is: " << endl;
     for(int i = 0; i <= n; i++){
        cout << a[i] << " ";
