393375545aefe94f836ecf5faee7bfd85894fd47
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-25 14:24:42 +0530

    Letter Printing
    25 July 2025

--- Programs/patterns/pattern14.cpp
+++ Programs/patterns/pattern14.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void pattern14(int n){
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<n;i++){
         for(char ch = 'A';ch<='A'+i;ch++){
             cout << ch << " ";
         }
