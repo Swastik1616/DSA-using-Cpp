0bfcd38c4642d4a2979a8a74f36fd822dd81ade1
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-25 14:22:20 +0530

    Done

--- Programs/patterns/pattern14.cpp
+++ Programs/patterns/pattern14.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void pattern14(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout << j;
+    for(int i=0;i<=n;i++){
+        for(char ch = 'A';ch<='A'+i;ch++){
+            cout << ch << " ";
         }
         cout << endl;
     }
