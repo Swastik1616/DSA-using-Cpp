aa6c306b418cd8ed45267a80ff77171b6db09b6b
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-25 17:02:34 +0530

    Reverse Letter Triangle
    25 July 2025

--- Programs/patterns/pattern15.cpp
+++ Programs/patterns/pattern15.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void pattern15(int n){
     for(int i=n;i>=1;i--){
         for(char ch='A'; ch<'A'+ i; ch++){
-            cout << ch;
+            cout << ch << " ";
         }
         cout << endl;
     }
