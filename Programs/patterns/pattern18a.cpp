102951e43877f67b23e3942357536cc93c516aa2
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-27 16:03:30 +0530

    update

--- Programs/patterns/pattern18a.cpp
+++ Programs/patterns/pattern18a.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pattern18(int n){
+void pattern18a(int n){
     for(int i=0;i<n;i++){
         for(char ch='A'+ n-1-i;ch<='A'+ n-1 ;ch++){
             cout << ch << " ";
@@ -13,5 +13,5 @@ void pattern18(int n){
 int main(){
     int n;
     cin >> n;
-    pattern18(n);
+    pattern18a(n);
 }
