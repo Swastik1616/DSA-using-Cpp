5ad6e165403a3272582997170049a5c818a4e732
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-27 15:45:46 +0530

    DAMN

--- Programs/patterns/pattern18.cpp
+++ Programs/patterns/pattern18.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void pattern18(int n){
-    for(int i=1;i<=n;i++){
-        for(char ch='A';ch<=ch + i;ch++){
-            cout << ch;
+    for(int i=0;i<n;i++){
+        for(char ch='A'+ n-1-i;ch<='A'+ n-1 ;ch++){
+            cout << ch << " ";
         }
         cout << endl;
     }
