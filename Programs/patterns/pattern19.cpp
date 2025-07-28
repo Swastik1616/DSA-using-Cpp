731bbe2dacaa68db6dbe5a0d89f8f706700086ff
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-28 18:04:30 +0530

    upper half done

--- Programs/patterns/pattern19.cpp
+++ Programs/patterns/pattern19.cpp
@@ -3,9 +3,24 @@ using namespace std;
 
 void pattern19(int n){
     for(int i=n;i>=1;i--){
+        
+        //stars
         for(int j=1;j<=i;j++){
             cout << "*";
         }
+
+        //spaces
+        for(int j=0;j<2*(n-i);j++){
+            cout << "-";
+        }
+
+
+        //stars
+         for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+
+
         cout << endl;
     }
 }
