963fc4e632b914b6940dba9a29eeed0a91eaa94c
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-28 18:07:23 +0530

    Done
    28 July 2025
    Radhe Radhe

--- Programs/patterns/pattern19.cpp
+++ Programs/patterns/pattern19.cpp
@@ -2,25 +2,36 @@
 using namespace std;
 
 void pattern19(int n){
+    //upper half
     for(int i=n;i>=1;i--){
-        
         //stars
         for(int j=1;j<=i;j++){
             cout << "*";
         }
-
         //spaces
         for(int j=0;j<2*(n-i);j++){
-            cout << "-";
+            cout << " ";
         }
-
-
         //stars
          for(int j=1;j<=i;j++){
             cout << "*";
         }
-
-
+        cout << endl;
+    }
+    //lower half
+    for(int i=1;i<=n;i++){
+        //stars
+        for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        //spaces
+        for(int j=2*(n-i);j>0;j--){
+            cout << " ";
+        }
+        //stars
+         for(int j=1;j<=i;j++){
+            cout << "*";
+        }
         cout << endl;
     }
 }
