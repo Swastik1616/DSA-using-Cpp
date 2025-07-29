d833113e3051770782324a025dd67c8fc0f970e5
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-29 20:54:57 +0530

    Update string_manipulation.cpp

--- Programs/basics/string_manipulation.cpp
+++ Programs/basics/string_manipulation.cpp
@@ -11,4 +11,23 @@ int main(){
     str1.append(str2);
     cout << str1 << endl;
     cout << "Cleary, the comment is proved!" << endl;
+
+    /*transfom function of algorithms library 
+    cane be used with ::to upper to convert the
+    characters of the string to uppercase*/
+    
+    // Headline text
+    string headline = "breaking news: new discovery";
+
+    // Convert the headline to uppercase
+              //start from here  //end at this  //store from here
+    transform(headline.begin(), headline.end(), headline.begin(), ::toupper);
+    
+    // Display the uppercase headline
+    cout << "Uppercase Headline -  " << headline << endl;
+
+    /*similarly use :tolower to 
+    convert to lower case*/
+    transform(headline.begin(), headline.end(), headline.begin(), ::tolower);
+    cout << "Lowercase Headline -  " << headline << endl;
 }
