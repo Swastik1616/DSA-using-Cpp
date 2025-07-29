7e5d8d149a98db57a3582d8360e5845087f17c08
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-29 19:43:59 +0530

    Create pattern20.cpp
    29 July 2025
    Radhe Radhe

---
+++ Programs/patterns/pattern20.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// just interchanged the upper hald and lower half loops of pattern 19
+void pattern20(int n){
+    //upper half
+    for(int i=1;i<n;i++){
+        //stars
+        for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        //spaces
+        for(int j=2*(n-i);j>0;j--){
+            cout << " ";
+        }
+        //stars
+         for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    //lower half
+    for(int i=n;i>=1;i--){
+        //stars
+        for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        //spaces
+        for(int j=0;j<2*(n-i);j++){
+            cout << " ";
+        }
+        //stars
+         for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    pattern20(n);
+}
