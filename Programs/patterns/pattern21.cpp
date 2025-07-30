44d13974b8e70120f3bfa78d15ba078d2f184a01
Swastik555 <swastikgondhi2204@gmail.com>
2025-07-30 18:38:21 +0530

    Update pattern21.cpp

--- Programs/patterns/pattern21.cpp
+++ Programs/patterns/pattern21.cpp
@@ -1,22 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
+// method 1
+// void pattern20(int n){
+//     for(int i=1;i<=n;i++){
+//         if(i==1 || i==n){
+//             for(int j=1;j<=n;j++){
+//                 cout << "*";
+//             }
+//         }
+//         else{
+//             for(int j=1;j<=n;j++){
+//                 if(j==1 || j==n){
+//                     cout << "*";
+//                 }
+//                 else{
+//                     cout << " ";
+//                 }
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
 
+//method 2 - same logic as above method, just combined them and optimized it
 void pattern20(int n){
     for(int i=1;i<=n;i++){
-        if(i==1 || i==n){
-            for(int j=1;j<=n;j++){
+        for(int j=1;j<=n;j++){
+            if(i == 1 || j==1 || i == n || j==n)
                 cout << "*";
-            }
-        }
-        else{
-            for(int j=1;j<=n;j++){
-                if(j==1 || j==n){
-                    cout << "*";
-                }
-                else{
-                    cout << " ";
-                }
-            }
+            else    
+                cout << " ";
         }
         cout << endl;
     }
