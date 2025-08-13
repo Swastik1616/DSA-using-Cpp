bd58985a82b72997cd57db156fde7340b60378b5
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-13 23:07:03 +0530

    Create WordEncryptor.cpp
    13 August 2025
    Radhe Radhe

---
+++ Programs/basics/WordEncryptor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    
+    for (int i = 0; i < word.length(); i++) {
+        char ch = word[i];  
+
+        if (ch >= '0' && ch <= '9')
+            word[i] = '*';
+            
+        else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                 ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            word[i] = '#';
+            
+        else if (ch >= 'A' && ch <= 'Z')
+            word[i] = '@';
+    }
+    cout << word << endl;
+
+    return 0;
+}
+// expected output = *s@#st#@*
