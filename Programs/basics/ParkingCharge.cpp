bad63ab253adcad15f97a4ce623fdad033d50797
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-24 19:03:53 +0530

    Create ParkingCharge.cpp
    24 August 2025
    Radhe Radhe

---
+++ Programs/basics/ParkingCharge.cpp
@@ -0,0 +1,13 @@
+/* Problem Link - 
+https://www.codechef.com/practice/course/cpp/PCPP02/problems/PARKINGCHARG
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y,h;
+    cin >> x >> y >> h ;
+    cout << x + y * (h-1);
+}
