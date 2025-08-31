a1e18d9792abfc18e8ab8d0f108460cffbf66d9c
Swastik555 <swastikgondhi2204@gmail.com>
2025-08-31 20:40:19 +0530

    Create LongestNonZsubA.cpp
    31 August 2025
    Radhe Radhe

---
+++ Programs/basics/LongestNonZsubA.cpp
@@ -0,0 +1,27 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/cpp/LPCPAS12/problems/LCPPAS163?tab=statement
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	int maxi = 0;
+	int cnt = 0;
+	for(int i = 0; i < n; i++){
+	    cin >> arr[i];
+	}
+	for(int i = 0; i < n; i++){
+	    if(arr[i] == 0){
+	        cnt = 0;
+	    }
+	    else{
+	        cnt++;
+	    }
+	    maxi = max(maxi, cnt);
+	}
+	cout<<maxi<<"\n";
+}
