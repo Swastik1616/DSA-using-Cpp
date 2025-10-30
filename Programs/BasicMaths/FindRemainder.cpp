24830c865c7c76b7b50a04b1b10d9f2007ddd06f
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-30 18:50:42 +0530

    Create FindRemainder.cpp
    30 October 2025
    Radhe Radhe

---
+++ Programs/BasicMaths/FindRemainder.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-math/BASICMATH/problems/FLOW002
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        cout << a%b << endl;
+    }
+}
