84f8dc8bdcb480f8ef8c68f0588299e8ca36d9ad
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-30 18:50:55 +0530

    Create MinimumNumberOf1s.cpp
    30 October 2025
    Radhe Radhe

---
+++ Programs/BasicMaths/MinimumNumberOf1s.cpp
@@ -0,0 +1,18 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-math/BASICMATHPRO/problems/MINNOO
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        // your code goes here
+        cout << n/2 << endl;
+    }
+
+}
