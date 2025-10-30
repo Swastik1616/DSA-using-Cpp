a967a09ae192504b89821f27f223ab50e158e214
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-30 18:50:50 +0530

    Create IntegerSum.cpp
    30 October 2025
    Radhe Radhe

---
+++ Programs/BasicMaths/IntegerSum.cpp
@@ -0,0 +1,13 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-math/BASICMATHPRO/problems/SUMNEQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+	// your code goes here
+    cout << (n-1) ;
+}
