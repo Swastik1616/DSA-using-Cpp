799cc1e1a19257022071787786b9f050abae5dc3
Swastik555 <swastikgondhi2204@gmail.com>
2025-10-30 18:50:45 +0530

    Create Fitness.cpp
    30 October 2025
    Radhe Radhe

---
+++ Programs/BasicMaths/Fitness.cpp
@@ -0,0 +1,17 @@
+/* Problem Link -
+https://www.codechef.com/practice/course/basic-math/BASICMATH/problems/FIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        cout << 10*x << endl;
+    }
+}
