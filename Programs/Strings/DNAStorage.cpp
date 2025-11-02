a10b731a743fbf7b656d1d7acbc81c47f1645e4c
Swastik555 <swastikgondhi2204@gmail.com>
2025-11-02 18:37:27 +0530

    Update DNAStorage.cpp
    2 November 2025
    Radhe Radhe

--- Programs/Strings/DNAStorage.cpp
+++ Programs/Strings/DNAStorage.cpp
@@ -2,3 +2,27 @@
 https://www.codechef.com/practice/course/strings/STRINGS/problems/DNASTORAGE
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        // your code goes here
+        if(n%2 == 0){
+            for(int i=0;i<n;i+=2){
+                if(s[i] == '0' && s[i+1] == '0') cout << "A";
+                if(s[i] == '0' && s[i+1] == '1') cout << "T";
+                if(s[i] == '1' && s[i+1] == '0') cout << "C";
+                if(s[i] == '1' && s[i+1] == '1') cout << "G";
+            }
+        }
+        cout << endl;
+    }
+
+}
