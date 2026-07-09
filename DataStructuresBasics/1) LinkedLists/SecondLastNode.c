ad92e0c6b06be76b3c5deae1def2d70235efbf6b
Swastik555 <swastikgondhi2204@gmail.com>
2026-07-09 15:51:55 +0530

    Create SecondLastNode.c
    9 July 2026
    Radhe Radhe

---
+++ DataStructuresBasics/1) LinkedLists/SecondLastNode.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <stdbool.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+}*head = NULL;
+
+// struct Node* head;
+
+void insertAtEnd(int x){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+
+    if(head==NULL) head = temp;
+    else{
+        struct Node* ptr = head;
+        while(ptr->next!=NULL){
+            ptr = ptr->next;
+        }
+        ptr->next = temp;
+    }
+}
+
+int printLL(struct Node *p){
+    if(head==NULL) return 0;
+    while(p->next!=NULL){
+        printf("%d->",p->data);
+        p = p->next;
+    }
+    printf("%d\n",p->data);
+    return 0;
+}
+
+int secondLastNode(struct Node* head){
+    if(head == NULL || head->next == NULL) return 0;
+    while(head->next->next != NULL) head = head->next;
+    return head->data;
+}
+
+int main(){
+    // head = NULL;
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        int x;
+        scanf("%d",&x);
+        insertAtEnd(x);
+    }
+   
+    printf("The Linked List is : ");
+    printLL(head);
+    
+    printf("Data of Second Last Node is : %d\n", secondLastNode(head));
+
+    return 0; 
+}
